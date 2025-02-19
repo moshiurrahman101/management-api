578ab1da807d809aa2e1be147bcfaac81d5366ad
moshiurrahman101 <99rmhridoy@gmail.com>
2025-02-19 19:21:08 +0600

    First commit

---
+++ server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Database connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+
+// Basic Route list
+app.get("/", (req, res) => {
+    res.send("Welcome to the management API");
+});
+
+// Starting Server
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
